d3b882f756695556e6e59be0328bf625d9d6e886
Brian Samson <brian@briansamson.com>
2015-02-12 13:13:15 -0600

    add support for delivering notifications through the development APNs gateway

--- lib/cloud_five_push.rb
+++ lib/cloud_five_push.rb
@@ -11,19 +11,30 @@ module CloudFivePush
     @api_key
   end
 
+  @aps_environment = nil
+  def self.aps_environment=(aps_environment)
+    @aps_environment = aps_environment
+  end
+
+  def self.aps_environment
+    @aps_environment
+  end
+
   def self.broadcast!(alert, scheduled_at = nil)
     notification = CloudFivePush::Notification.new
     notification.broadcast = true
     notification.alert = alert
     notification.scheduled_at = scheduled_at
+    notification.aps_environment = @aps_environment
     notification.notify!
   end
 
-  def self.notify!(alert, user_identifers, scheduled_at = nil)
+  def self.notify!(alert, user_identifiers, scheduled_at = nil)
     notification = CloudFivePush::Notification.new
     notification.alert = alert
     notification.user_identifiers = user_identifiers
     notification.scheduled_at = scheduled_at
+    notification.aps_environment = @aps_environment
     notification.notify!
   end
 end
--- spec/notification_spec.rb
+++ spec/notification_spec.rb
@@ -1,18 +1,25 @@
 require 'spec_helper'
 
 describe CloudFivePush::Notification do
-  before(:all) { CloudFivePush.api_key = "fake" }
-  context "Setting user identifiers" do 
-    let(:notification) { CloudFivePush::Notification.new }
-    it "accepts an array of identifiers" do 
+  before(:all) { CloudFivePush.api_key = "fake"; }
+  let(:notification) { CloudFivePush::Notification.new }
+  context "Setting user identifiers" do
+    it "accepts an array of identifiers" do
       notification.user_identifiers = [1,2,3]
       expect(notification.user_identifiers).to eq [1,2,3]
     end
 
-    it "accepts a single user_identifier" do 
+    it "accepts a single user_identifier" do
       notification.user_identifiers = 1
       expect(notification.user_identifiers).to eq [1]
     end
+  end
+
+
+  it "Uses the global aps_environment setting" do
+    CloudFivePush.aps_environment = :development
+    expect(notification.aps_environment).to eq :development
+    CloudFivePush.aps_environment = nil #reset
   end
 
 end
